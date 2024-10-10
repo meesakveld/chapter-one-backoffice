4417c36df7438f2bc96efb56910a2f2cbd63ec97
pgm-meesakveld <mees.akveld@student.arteveldehs.be>
2024-10-10 14:27:53 +0200

    Added models

--- views/home.php
+++ views/home.php
@@ -1,2 +1,13 @@
 <h1>Base MVC</h1>
 <p>Welcome to this base mvc project.</p>
+
+<ul>
+<?php
+    foreach($books as $book) {
+        echo '<ul>';
+            echo '<h2>' . $book->title . '</h2>';
+            echo '<p>€' . $book->price . '</p>';
+        echo '</ul>';
+    }
+?>
+</ul>
